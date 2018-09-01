15fc9f4a2e4e3c548b582a5c89b27bc823e8aeae
Rana Emad <rana3emad@gmail.com>
2018-09-01 19:47:40 +0200

    Controller tasks function

--- application/controllers/Tasks.php
+++ application/controllers/Tasks.php
@@ -6,4 +6,11 @@ class Tasks extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 	}
+	public function index(){
+		$tasks= $this->Crud_model->get_all("tasks");
+		$data['title']= "Tasks";
+		$data['view_path']="tasks/tasks";
+		$data['tasks']=$tasks;
+		$this->load->view("index",$data);
+	}
 }
