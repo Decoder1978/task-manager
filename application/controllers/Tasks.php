54350285ea69025e4426f29f7554d4be6259905a
Rana Emad <rana3emad@gmail.com>
2018-09-14 13:01:35 +0200

    Controller edit task function

--- application/controllers/Tasks.php
+++ application/controllers/Tasks.php
@@ -38,4 +38,29 @@ class Tasks extends CI_Controller {
 			redirect(base_url("Tasks"));
 		}
 	}
+	public function edit_task($id){
+		$this->form_validation->set_rules("task","Task","trim|required");
+		$this->form_validation->set_rules("start_date","Start Date","trim");
+		$this->form_validation->set_rules("deadline","Deadline","trim");
+		$this->form_validation->set_rules("status","Status","trim");
+		$this->form_validation->set_rules("notes","Notes","trim");
+		if(!$this->form_validation->run()){
+			$data['title']= "Edit Task";
+			$data['view_path']="tasks/edit_task";
+			$this->load->view("index",$data);
+		}
+		else{
+			$data=array(
+				"task"=>$this->input->post("task"),
+				"start_date"=>$this->input->post("start_date")?date("Y-m-d H:i:s",strtotime($this->input->post("start_date"))):"",
+				"deadline"=>$this->input->post("deadline")?date("Y-m-d H:i:s",strtotime($this->input->post("deadline"))):"",
+				"status"=>$this->input->post("status"),
+				"notes"=>$this->input->post("notes")
+			);
+			$this->Crud_model->update("tasks",$id,$data);
+			$this->session->set_flashdata("success","Task Updated Successfully!");
+			redirect(base_url("Tasks"));
+		}
+	}
+
 }
