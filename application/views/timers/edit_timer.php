31d8fed93c01b7781b963b3c42993e3a9de40cfa
Rana Emad <rana3emad@gmail.com>
2018-10-04 17:27:33 +0200

    Edit timer: view

---
+++ application/views/timers/edit_timer.php
@@ -0,0 +1,16 @@
+<div class="container">
+  <form method="POST" action="">
+    <div class="form-group">
+      <label for="start">Start</label>
+      <input name="start" type="datetime-local" class="form-control" id="start" value="<?=$timer->start?>" >
+    </div>
+
+    <div class="form-group">
+      <label for="end">End</label>
+      <input name="end" type="datetime-local" class="form-control" id="end" value="<?=$timer->end?>">
+    </div>
+
+    <button type="submit" class="btn btn-secondary">Submit</button>
+  </form>
+
+</div>
